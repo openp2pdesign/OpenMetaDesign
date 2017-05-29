319dc26a651f987cdc8b2190d94fffc272367551
Massimo Menichinelli <info@openp2pdesign.org>
2017-05-29 23:03:48 +0200

    Fix tabular bootstrap in users template

--- app/client/templates/users/users.js
+++ app/client/templates/users/users.js
@@ -22,3 +22,9 @@ Template.Users.onCreated(function() {
 Template.Users.onRendered(function() {});
 
 Template.Users.onDestroyed(function() {});
+
+// Setup of tabular for this template
+import { $ } from 'meteor/jquery';
+import dataTablesBootstrap from 'datatables.net-bs';
+import 'datatables.net-bs/css/dataTables.bootstrap.css';
+dataTablesBootstrap(window, $);
