2e5da568fd8bc1feb8028726aaffb78a5d089fb6
Massimo Menichinelli <info@openp2pdesign.org>
2017-06-11 15:34:06 +0200

    Second configuration of datepicker for autoform, with custom template

---
+++ app/client/templates/forms/datepicker/datepicker.js
@@ -0,0 +1,26 @@
+/*****************************************************************************/
+/* Datepicker: Event Handlers */
+/*****************************************************************************/
+Template.Datepicker.events({
+});
+
+/*****************************************************************************/
+/* Datepicker: Helpers */
+/*****************************************************************************/
+Template.Datepicker.helpers({
+});
+
+/*****************************************************************************/
+/* Datepicker: Lifecycle Hooks */
+/*****************************************************************************/
+Template.Datepicker.onCreated(function () {
+});
+
+Template.Datepicker.onRendered(function () {
+});
+
+Template.Datepicker.onDestroyed(function () {
+});
+
+// Replace the Autoform datepicker with this one
+Template.Datepicker.replaces("afBootstrapDatepicker");
