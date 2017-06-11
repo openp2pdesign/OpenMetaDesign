31b8d4ecb571c7c7fb74dcc272ccbd37242251ef
Massimo Menichinelli <info@openp2pdesign.org>
2017-06-11 13:24:12 +0200

    Move user delete confirmation template to separate files

--- app/client/templates/edit_user/edit_user.js
+++ app/client/templates/edit_user/edit_user.js
@@ -2,29 +2,12 @@
 /* editUser: Event Handlers */
 /*****************************************************************************/
 
-userToBeDeleted = "";
-
 Template.editUser.events({
     'click .edit-user': function() {
         console.log("edit", this._id);
     },
     'click .delete-user': function() {
-        userToBeDeleted = this._id;
-        Modal.show('confirmUserDelete', this);
-    }
-});
-
-Template.confirmUserDelete.events({
-    'click #confirm': function() {
-        Meteor.users.remove({
-            _id: this._id
-        }, function(error, result) {
-            if (error) {
-                console.log("Error removing user: ", error);
-            } else {
-                console.log("Number of users removed: " + result);
-            }
-        })
+        Modal.show('ConfirmUserDelete', this);
     }
 });
 
---
+++ app/client/templates/modals/confirm_user_delete/confirm_user_delete.js
@@ -0,0 +1,34 @@
+/*****************************************************************************/
+/* ConfirmUserDelete: Event Handlers */
+/*****************************************************************************/
+Template.ConfirmUserDelete.events({
+    'click #confirm': function() {
+        Meteor.users.remove({
+            _id: this._id
+        }, function(error, result) {
+            if (error) {
+                console.log("Error removing user: ", error);
+            } else {
+                console.log("Number of users removed: " + result);
+            }
+        })
+    }
+});
+
+/*****************************************************************************/
+/* ConfirmUserDelete: Helpers */
+/*****************************************************************************/
+Template.ConfirmUserDelete.helpers({
+});
+
+/*****************************************************************************/
+/* ConfirmUserDelete: Lifecycle Hooks */
+/*****************************************************************************/
+Template.ConfirmUserDelete.onCreated(function () {
+});
+
+Template.ConfirmUserDelete.onRendered(function () {
+});
+
+Template.ConfirmUserDelete.onDestroyed(function () {
+});
