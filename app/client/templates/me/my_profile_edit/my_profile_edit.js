47223452eb7f85fa2dcb55dc02ab536226bdd43c
Massimo Menichinelli <info@openp2pdesign.org>
2017-06-23 15:31:11 +0200

    Add notifications for user edit

--- app/client/templates/me/my_profile_edit/my_profile_edit.js
+++ app/client/templates/me/my_profile_edit/my_profile_edit.js
@@ -1,6 +1,13 @@
 /*****************************************************************************/
 /* MyProfileEdit: Event Handlers */
 /*****************************************************************************/
+
+// Load Pnotify
+import 'pnotify/dist/pnotify.css';
+import PNotify from 'pnotify';
+PNotify.prototype.options.styling = "bootstrap3";
+PNotify.prototype.options.styling = "fontawesome";
+
 Template.MyProfileEdit.events({
     'click #confirm': function(event) {
         event.preventDefault();
@@ -10,9 +17,10 @@ Template.MyProfileEdit.events({
         var newEmail = $('#new-email').val();
         var newBio = $('#new-bio').val();
 
+
         // Validate and save new data
 
-        if (newFirstName) {
+        if ((newFirstName) && (newFirstName != Meteor.user().profile.firstName)) {
             Meteor.users.update({
                 _id: Meteor.userId()
             }, {
@@ -20,8 +28,28 @@ Template.MyProfileEdit.events({
                     "profile.firstName": newFirstName
                 }
             });
+            var successNotice = new PNotify({
+                type: 'success',
+                title: 'Success',
+                text: 'Your name was successfully updated.',
+                icon: 'fa fa-user',
+                addclass: 'pnotify stack-topright',
+                animate: {
+                    animate: true,
+                    in_class: 'slideInDown',
+                    out_class: 'slideOutUp'
+                },
+                buttons: {
+                    closer: true,
+                    sticker: false
+                }
+            });
+            successNotice.get().click(function() {
+                successNotice.remove();
+            });
         }
-        if (newLastName) {
+
+        if ((newLastName) && (newLastName != Meteor.user().profile.lastName)) {
             Meteor.users.update({
                 _id: Meteor.userId()
             }, {
@@ -29,8 +57,28 @@ Template.MyProfileEdit.events({
                     "profile.lastName": newLastName
                 }
             });
+            var successNotice = new PNotify({
+                type: 'success',
+                title: 'Success',
+                text: 'Your surname was successfully updated.',
+                icon: 'fa fa-user',
+                addclass: 'pnotify stack-topright',
+                animate: {
+                    animate: true,
+                    in_class: 'slideInDown',
+                    out_class: 'slideOutUp'
+                },
+                buttons: {
+                    closer: true,
+                    sticker: false
+                }
+            });
+            successNotice.get().click(function() {
+                successNotice.remove();
+            });
         }
-        if (newBio) {
+
+        if ((newBio) && (newBio != Meteor.user().profile.bio)) {
             Meteor.users.update({
                 _id: Meteor.userId()
             }, {
@@ -38,19 +86,77 @@ Template.MyProfileEdit.events({
                     "profile.bio": newBio
                 }
             });
-        }
-        if ((newEmail) && (validateEmail(newEmail))) {
-            Meteor.users.update({
-                _id: Meteor.userId()
-            }, {
-                $set: {
-                    "emails.0.address": newEmail
+            var successNotice = new PNotify({
+                type: 'success',
+                title: 'Success',
+                text: 'Your bio was successfully updated.',
+                icon: 'fa fa-user',
+                addclass: 'pnotify stack-topright',
+                animate: {
+                    animate: true,
+                    in_class: 'slideInDown',
+                    out_class: 'slideOutUp'
+                },
+                buttons: {
+                    closer: true,
+                    sticker: false
                 }
             });
-        } else {
-            // show error
+            successNotice.get().click(function() {
+                successNotice.remove();
+            });
         }
 
+        if ((newEmail) && (newEmail != Meteor.user().emails[0].address)) {
+            if (validateEmail(newEmail)) {
+                Meteor.users.update({
+                    _id: Meteor.userId()
+                }, {
+                    $set: {
+                        "emails.0.address": newEmail
+                    }
+                });
+                var successNotice = new PNotify({
+                    type: 'success',
+                    title: 'Success',
+                    text: 'Your e-mail was successfully updated.',
+                    icon: 'fa fa-user',
+                    addclass: 'pnotify stack-topright',
+                    animate: {
+                        animate: true,
+                        in_class: 'slideInDown',
+                        out_class: 'slideOutUp'
+                    },
+                    buttons: {
+                        closer: true,
+                        sticker: false
+                    }
+                });
+                successNotice.get().click(function() {
+                    successNotice.remove();
+                });
+            } else {
+                var errorNotice = new PNotify({
+                    type: 'error',
+                    title: 'Error',
+                    text: 'There was a problem with your e-mail.',
+                    icon: 'fa fa-exclamation-triangle',
+                    addclass: 'pnotify stack-topright',
+                    animate: {
+                        animate: true,
+                        in_class: 'slideInDown',
+                        out_class: 'slideOutUp'
+                    },
+                    buttons: {
+                        closer: true,
+                        sticker: false
+                    }
+                });
+                errorNotice.get().click(function() {
+                    errorNotice.remove();
+                });
+            }
+        }
     }
 });
 
@@ -66,11 +172,8 @@ Template.MyProfileEdit.helpers({
 /*****************************************************************************/
 /* MyProfileEdit: Lifecycle Hooks */
 /*****************************************************************************/
-Template.MyProfileEdit.onCreated(function () {
-});
+Template.MyProfileEdit.onCreated(function() {});
 
-Template.MyProfileEdit.onRendered(function () {
-});
+Template.MyProfileEdit.onRendered(function() {});
 
-Template.MyProfileEdit.onDestroyed(function () {
-});
+Template.MyProfileEdit.onDestroyed(function() {});
