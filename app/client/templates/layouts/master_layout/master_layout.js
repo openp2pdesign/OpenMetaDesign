db4d706db2111571da1e37734b35e2d12f5b09ed
Massimo Menichinelli <info@openp2pdesign.org>
2017-06-23 12:34:43 +0200

    Integrate pnotify

--- app/client/templates/layouts/master_layout/master_layout.js
+++ app/client/templates/layouts/master_layout/master_layout.js
@@ -1,3 +1,16 @@
+// Load Pnotify
+import 'pnotify/dist/pnotify.css';
+import PNotify from 'pnotify';
+PNotify.prototype.options.styling = "bootstrap3";
+PNotify.prototype.options.styling = "fontawesome";
+
+var myStack = {
+    "dir1": "down",
+    "dir2": "right",
+    "push": "top"
+};
+
+
 Template.MasterLayout.helpers({});
 
 Template.MasterLayout.events({
@@ -6,5 +19,30 @@ Template.MasterLayout.events({
         Meteor.logout(function() {
             Router.go('/');
         });
+    },
+    // Just a test for Pnotify
+    'click #home': function(event) {
+        event.preventDefault();
+        
+        var notice = new PNotify({
+            type: 'info',
+            title: 'Title',
+            text: 'Text.',
+            icon: 'fa fa-envelope-o',
+            addclass: 'pnotify stack-topright',
+            animate: {
+                animate: true,
+                in_class: 'slideInDown',
+                out_class: 'slideOutUp'
+            },
+            buttons: {
+                closer: true,
+                sticker: false
+            }
+        });
+        notice.get().click(function() {
+            notice.remove();
+        });
+
     }
 });
