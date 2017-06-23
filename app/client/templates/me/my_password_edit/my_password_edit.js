e89a01083bf229b74b847b929135b706e03f3808
Massimo Menichinelli <info@openp2pdesign.org>
2017-06-23 13:17:24 +0200

    Add password reset for loggedin users

---
+++ app/client/templates/me/my_password_edit/my_password_edit.js
@@ -0,0 +1,23 @@
+/*****************************************************************************/
+/* MyPasswordEdit: Event Handlers */
+/*****************************************************************************/
+Template.MyPasswordEdit.events({
+});
+
+/*****************************************************************************/
+/* MyPasswordEdit: Helpers */
+/*****************************************************************************/
+Template.MyPasswordEdit.helpers({
+});
+
+/*****************************************************************************/
+/* MyPasswordEdit: Lifecycle Hooks */
+/*****************************************************************************/
+Template.MyPasswordEdit.onCreated(function () {
+});
+
+Template.MyPasswordEdit.onRendered(function () {
+});
+
+Template.MyPasswordEdit.onDestroyed(function () {
+});
