f3de4bc022b578f69aa981b59a2313d81ebeaacd
Massimo Menichinelli <info@openp2pdesign.org>
2017-07-30 18:18:40 +0200

    Start the i18n process, add packages, add gulp file and libraries for creating the translation files

---
+++ app/gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var i18next = require('i18next-parser');
+
+gulp.task('translate', function() {
+    gulp.src(['!node_modules/**', '**'])
+        .pipe(i18next({
+            output: 'i18n',
+            locales: ['en', 'es'],
+            functions: ['_'],
+            extension: ".json",
+            namespace: 'i18n',
+            prefix: '$LOCALE.',
+            writeOld: false
+        }))
+        .pipe(gulp.dest('i18n'));
+});
