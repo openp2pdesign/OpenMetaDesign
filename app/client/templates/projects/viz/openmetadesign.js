dfc3982864e325fd135e2202c25645fa222c9b3f
Massimo Menichinelli <info@openp2pdesign.org>
2017-08-23 16:34:08 +0200

    Add window onresize listener to viz

--- app/client/templates/projects/viz/openmetadesign.js
+++ app/client/templates/projects/viz/openmetadesign.js
@@ -1,6 +1,18 @@
 import d3 from 'd3';
 
 export default function openmetadesign_viz(data) {
+
+    // The container for the viz
+    var d3Container = document.getElementById("d3-container");
+
+    // Get dimensions of the container on window resize
+    window.addEventListener("resize", function(d) {
+        width = d3Container.clientWidth;
+        height = d3Container.clientHeight;
+        console.log(width, height);
+    });
+
+    // Add the SVG to the container
     var svg = d3.select('#d3-container').append("svg");
     svg.append("rect")
         .attr("x", 10)
