dee6f9910b1cb7df0684d9d1119dcccdc462681e
Massimo Menichinelli <info@openp2pdesign.org>
2017-12-16 18:07:11 +0100

    Add icons to tabs and add ActivityDelete template

---
+++ app/client/templates/modals/project/activity_delete/activity_delete.js
@@ -0,0 +1,23 @@
+/*****************************************************************************/
+/* ActivityDelete: Event Handlers */
+/*****************************************************************************/
+Template.ActivityDelete.events({
+});
+
+/*****************************************************************************/
+/* ActivityDelete: Helpers */
+/*****************************************************************************/
+Template.ActivityDelete.helpers({
+});
+
+/*****************************************************************************/
+/* ActivityDelete: Lifecycle Hooks */
+/*****************************************************************************/
+Template.ActivityDelete.onCreated(function () {
+});
+
+Template.ActivityDelete.onRendered(function () {
+});
+
+Template.ActivityDelete.onDestroyed(function () {
+});
