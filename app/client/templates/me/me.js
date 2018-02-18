0c55563af2a330d0f35428db7e82c4ed7be3ab30
Massimo Menichinelli <info@openp2pdesign.org>
2018-02-18 16:34:58 +0100

    Fix a bug in project creation in me page

--- app/client/templates/me/me.js
+++ app/client/templates/me/me.js
@@ -8,7 +8,7 @@ Template.Me.events({
     'click .change-password': function() {
         Modal.show('ChangePassword', this);
     },
-    'click .create-project': function(event, template) {
+    'click #create-project-button': function(event, template) {
         event.preventDefault();
         Meteor.call('createProject', function(error, result) {
             Router.go('projectsViz', {
