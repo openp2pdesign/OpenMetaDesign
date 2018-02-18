c589d88ba402e4c9021daa3c632aaffed3d43193
Massimo Menichinelli <info@openp2pdesign.org>
2018-02-18 13:55:36 +0100

    Almost working delete activity tab

--- app/client/templates/modals/project/activity_delete/activity_delete.js
+++ app/client/templates/modals/project/activity_delete/activity_delete.js
@@ -2,22 +2,81 @@
 /* ActivityDelete: Event Handlers */
 /*****************************************************************************/
 Template.ActivityDelete.events({
+    'click #delete-activity-button': function(event) {
+        event.preventDefault();
+        Meteor.call('deleteActivity', this.project._id, this.process.id, this.activity.id, function(error, result) {
+            if (error) {
+                var errorNotice = new PNotify({
+                    type: 'error',
+                    title: 'Error',
+                    text: 'There was an error in deleting the activity',
+                    icon: 'fa fa-random',
+                    addclass: 'pnotify stack-topright',
+                    animate: {
+                        animate: true,
+                        in_class: 'slideInDown',
+                        out_class: 'slideOutUp'
+                    },
+                    buttons: {
+                        closer: true,
+                        sticker: false
+                    }
+                });
+
+                errorNotice.get().click(function() {
+                    errorNotice.remove();
+                });
+            } else {
+                // Close the modal, since there is no activity any longer
+                Modal.hide('Activity');
+                var successNotice = new PNotify({
+                    type: 'success',
+                    title: 'Success',
+                    text: 'Activity successfully deleted.',
+                    icon: 'fa fa-randoms',
+                    addclass: 'pnotify stack-topright',
+                    animate: {
+                        animate: true,
+                        in_class: 'slideInDown',
+                        out_class: 'slideOutUp'
+                    },
+                    buttons: {
+                        closer: true,
+                        sticker: false
+                    }
+                });
+
+                successNotice.get().click(function() {
+                    successNotice.remove();
+                });
+            }
+        });
+    },
+    'click #cancel-delete-activity-button': function(event) {
+        event.preventDefault();
+        $('.nav-tabs a[href="#activity-view-tab"]').tab('show');
+    }
 });
 
 /*****************************************************************************/
 /* ActivityDelete: Helpers */
 /*****************************************************************************/
 Template.ActivityDelete.helpers({
+    data: function() {
+        // Return helper values for the template
+        return {
+            "project": thisProject,
+            "process": this.process,
+            "activity": thisActivity
+        }
+    }
 });
 
 /*****************************************************************************/
 /* ActivityDelete: Lifecycle Hooks */
 /*****************************************************************************/
-Template.ActivityDelete.onCreated(function () {
-});
+Template.ActivityDelete.onCreated(function() {});
 
-Template.ActivityDelete.onRendered(function () {
-});
+Template.ActivityDelete.onRendered(function() {});
 
-Template.ActivityDelete.onDestroyed(function () {
-});
+Template.ActivityDelete.onDestroyed(function() {});
