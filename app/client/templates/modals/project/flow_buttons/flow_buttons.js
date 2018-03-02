d9dafbb5feed2f6d0e8c9dce5957ac01f9038ce9
Massimo Menichinelli <info@openp2pdesign.org>
2018-03-02 21:23:28 +0100

    First step towards implementing a flow visualization in D3

--- app/client/templates/modals/project/flow_buttons/flow_buttons.js
+++ app/client/templates/modals/project/flow_buttons/flow_buttons.js
@@ -1,3 +1,8 @@
+import { Activities } from '../../../../../lib/collections/activities.js';
+import { Flows } from '../../../../../lib/collections/flows.js';
+// Viz
+import d3 from 'd3';
+
 /*****************************************************************************/
 /* FlowButtons: Event Handlers */
 /*****************************************************************************/
@@ -34,6 +39,38 @@ Template.FlowButtons.events({
         $("#discussFlowDiv").hide();
         // Pass the flow id to Sessions
         Session.set('flowToShowData', this._id);
+
+        // Visualize the Flow with D3
+
+        // Margins
+        // https://bl.ocks.org/mbostock/3019563
+        var margin = {
+            top: 15,
+            right: 0,
+            bottom: 0,
+            left: 0
+        };
+
+        // Get dimensions of the container on window resize
+        window.addEventListener("resize", function(d) {
+            width = d3Container.clientWidth;
+            height = d3Container.clientHeight;
+            console.log(width, height);
+        });
+
+        // Remove previous SVG
+        d3.select('#showFlowDiv').select('svg').remove();
+
+        // Add the visualization SVG to the container
+        var svg = d3.select('#showFlowDiv').append("svg")
+            .attr("width", "100%")
+            .attr("height", "100%")
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        // Visualize the flow in the SVG
+        svg.append("circle").attr("cx", 30).attr("cy", 30).attr("r", 20);
+
     },
     // Show the div that enable the edit of flows
     'click .create-flow': function(event, template) {
