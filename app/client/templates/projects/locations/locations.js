c9dd3adcfd4416af8840d40f8fef8002dffc078b
Massimo Menichinelli <info@openp2pdesign.org>
2018-03-17 19:01:21 +0100

    Add leaflet, missing invalidateSize

--- app/client/templates/projects/locations/locations.js
+++ app/client/templates/projects/locations/locations.js
@@ -1,3 +1,7 @@
+// Import Leaflet
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+// Import collections
 import { Activities } from '../../../../lib/collections/activities.js';
 
 /*****************************************************************************/
@@ -23,6 +27,13 @@ Template.Locations.onCreated(function () {
 });
 
 Template.Locations.onRendered(function () {
+    var mymap = L.map('locationsMap').setView([51.505, -0.09], 13);
+    // Tiles: http://leaflet-extras.github.io/leaflet-providers/preview/#filter=Esri.WorldGrayCanvas
+    L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
+		maxZoom: 16,
+		attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
+	}).addTo(mymap);
+    map.invalidateSize();
 });
 
 Template.Locations.onDestroyed(function () {
