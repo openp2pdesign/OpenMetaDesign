0d92e9ebbda4c2b24fb3eabafd8ee64d14b8effe
Massimo Menichinelli <info@openp2pdesign.org>
2018-03-23 21:22:10 +0100

    Improve moving of flow and contradiction to own modal

--- app/client/templates/modals/project/contradiction/contradiction.js
+++ app/client/templates/modals/project/contradiction/contradiction.js
@@ -1,3 +1,22 @@
+// Load Pnotify
+import 'pnotify/dist/pnotify.css';
+import PNotify from 'pnotify';
+PNotify.prototype.options.styling = "bootstrap3";
+PNotify.prototype.options.styling = "fontawesome";
+// jquery
+import { $ } from 'meteor/jquery';
+// Random id
+import { Random } from 'meteor/random';
+// Load Projects and Settings
+import { Projects } from '../../../../../lib/collections/projects.js';
+import { Settings } from '../../../../../lib/collections/settings.js';
+import { Activities } from '../../../../../lib/collections/activities.js';
+import { ActivityElements } from '../../../../../lib/collections/activity_elements.js';
+import { Contradictions } from '../../../../../lib/collections/contradictions.js';
+
+// Client only collection for the autocomplete
+LocalActivityElements = new Mongo.Collection(null);
+
 /*****************************************************************************/
 /* Contradiction: Event Handlers */
 /*****************************************************************************/
@@ -17,6 +36,11 @@ Template.Contradiction.onCreated(function () {
 });
 
 Template.Contradiction.onRendered(function () {
+    // Add tooltip to the tabs
+    $('[data-toggle="tab"]').tooltip({
+        trigger: 'hover',
+        placement: 'top'
+    });
 });
 
 Template.Contradiction.onDestroyed(function () {
--- app/client/templates/modals/project/flow/flow.js
+++ app/client/templates/modals/project/flow/flow.js
@@ -1,3 +1,18 @@
+// Load Pnotify
+import 'pnotify/dist/pnotify.css';
+import PNotify from 'pnotify';
+PNotify.prototype.options.styling = "bootstrap3";
+PNotify.prototype.options.styling = "fontawesome";
+// jquery
+import { $ } from 'meteor/jquery';
+// Random id
+import { Random } from 'meteor/random';
+// Load Projects and Settings
+import { Projects } from '../../../../../lib/collections/projects.js';
+import { Settings } from '../../../../../lib/collections/settings.js';
+import { Activities } from '../../../../../lib/collections/activities.js';
+import { Flows } from '../../../../../lib/collections/flows.js';
+
 /*****************************************************************************/
 /* Flow: Event Handlers */
 /*****************************************************************************/
@@ -17,6 +32,11 @@ Template.Flow.onCreated(function () {
 });
 
 Template.Flow.onRendered(function () {
+    // Add tooltip to the tabs
+    $('[data-toggle="tab"]').tooltip({
+        trigger: 'hover',
+        placement: 'top'
+    });
 });
 
 Template.Flow.onDestroyed(function () {
