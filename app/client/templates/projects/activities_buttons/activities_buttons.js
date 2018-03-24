9da5d7cf7bc47bafb6b9260c94361484ebbc1286
Massimo Menichinelli <info@openp2pdesign.org>
2018-03-24 18:55:22 +0100

    Fix button for activity tabular

--- app/client/templates/projects/activities_buttons/activities_buttons.js
+++ app/client/templates/projects/activities_buttons/activities_buttons.js
@@ -1,7 +1,27 @@
+import { Activities } from '../../../../lib/collections/activities.js';
+
 /*****************************************************************************/
 /* ActivitiesButtons: Event Handlers */
 /*****************************************************************************/
 Template.ActivitiesButtons.events({
+    // Launch the modal for the activity
+    'click .edit-activity': function(event, template) {
+        event.preventDefault();
+        // Get IDs of project, process, activity
+        var thisActivityID = this._id;
+        var thisActivity = Activities.findOne({ '_id':thisActivityID });
+        var thisProjectID = thisActivity.projectId;
+        var thisProcessID = thisActivity.processId;
+        // Launch the modal
+        Modal.show('Activity', function() {
+            return {
+                "project": thisProjectID,
+                "process": thisProcessID,
+                "activity": thisActivityID,
+                "mode": "edit"
+            }
+        });
+    },
 });
 
 /*****************************************************************************/
