e33423b4dfc9922f2023f7c31f4e3177a1a3f93e
Massimo Menichinelli <info@openp2pdesign.org>
2018-03-30 09:53:47 +0200

    Separate flow view from flow modal

--- app/client/templates/modals/project/flow/flow.js
+++ app/client/templates/modals/project/flow/flow.js
@@ -5,8 +5,6 @@ PNotify.prototype.options.styling = "bootstrap3";
 PNotify.prototype.options.styling = "fontawesome";
 // jquery
 import { $ } from 'meteor/jquery';
-// Viz
-import d3 from 'd3';
 // Load Projects and Settings
 import { Projects } from '../../../../../lib/collections/projects.js';
 import { Settings } from '../../../../../lib/collections/settings.js';
@@ -80,7 +78,7 @@ Template.Flow.helpers({
             'projectId': this._id
         }).fetch();
     },
-    flowData: function() {
+    data: function() {
         // Get the flow data
         var thisData = Flows.findOne({
             '_id': this.flowId
@@ -132,40 +130,6 @@ Template.Flow.onRendered(function() {
         trigger: 'hover',
         placement: 'top'
     });
-
-    // Visualize the Flow with D3
-
-    // Margins
-    // https://bl.ocks.org/mbostock/3019563
-    var margin = {
-        top: 15,
-        right: 0,
-        bottom: 0,
-        left: 0
-    };
-
-    // The container for the viz
-    var d3Container = document.getElementById("showFlowDivSVG");
-
-    // Get dimensions of the container on window resize
-    window.addEventListener("resize", function(d) {
-        width = d3Container.clientWidth;
-        height = d3Container.clientHeight;
-        console.log(width, height);
-    });
-
-    // Remove previous SVG
-    d3.select('#showFlowDivSVG').select('svg').remove();
-
-    // Add the visualization SVG to the container
-    var svg = d3.select('#showFlowDivSVG').append("svg")
-        .attr("width", "100%")
-        .attr("height", "100%")
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    // Visualize the flow in the SVG
-    svg.append("circle").attr("cx", 30).attr("cy", 30).attr("r", 20);
 });
 
 Template.Flow.onDestroyed(function() {});
---
+++ app/client/templates/modals/project/flow_view/flow_view.js
@@ -0,0 +1,90 @@
+// jquery
+import { $ } from 'meteor/jquery';
+// Viz
+import d3 from 'd3';
+// Load Projects and Settings
+import { Projects } from '../../../../../lib/collections/projects.js';
+import { Settings } from '../../../../../lib/collections/settings.js';
+import { Activities } from '../../../../../lib/collections/activities.js';
+import { Flows } from '../../../../../lib/collections/flows.js';
+
+/*****************************************************************************/
+/* FlowView: Event Handlers */
+/*****************************************************************************/
+Template.FlowView.events({
+});
+
+/*****************************************************************************/
+/* FlowView: Helpers */
+/*****************************************************************************/
+Template.FlowView.helpers({
+    data: function() {
+        // Get the flow data
+        var thisData = Flows.findOne({
+            '_id': this.flowId
+        });
+        // If there is data... then return it
+        if (thisData) {
+            // Add the data for the nodes
+            thisData.firstNodeData = Activities.findOne({
+                '_id': thisData.flowData.firstNode
+            });
+            thisData.secondNodeData = Activities.findOne({
+                '_id': thisData.flowData.secondNode
+            });
+            // Return the data
+            return thisData;
+        }
+    },
+});
+
+/*****************************************************************************/
+/* FlowView: Lifecycle Hooks */
+/*****************************************************************************/
+Template.FlowView.onCreated(function () {
+    Meteor.subscribe('projects');
+    Meteor.subscribe('activities');
+    Meteor.subscribe('flows');
+    // Load variables
+    thisProjectID = this.data.projectId;
+    thisFlowID = this.data.flowId;
+});
+
+Template.FlowView.onRendered(function () {
+    // Visualize the Flow with D3
+
+    // Margins
+    // https://bl.ocks.org/mbostock/3019563
+    var margin = {
+        top: 15,
+        right: 0,
+        bottom: 0,
+        left: 0
+    };
+
+    // The container for the viz
+    var d3Container = document.getElementById("showFlowDivSVG");
+
+    // Get dimensions of the container on window resize
+    window.addEventListener("resize", function(d) {
+        width = d3Container.clientWidth;
+        height = d3Container.clientHeight;
+        console.log(width, height);
+    });
+
+    // Remove previous SVG
+    d3.select('#showFlowDivSVG').select('svg').remove();
+
+    // Add the visualization SVG to the container
+    var svg = d3.select('#showFlowDivSVG').append("svg")
+        .attr("width", "100%")
+        .attr("height", "100%")
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    // Visualize the flow in the SVG
+    svg.append("circle").attr("cx", 30).attr("cy", 30).attr("r", 20);
+});
+
+Template.FlowView.onDestroyed(function () {
+});
